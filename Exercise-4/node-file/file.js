05e7d00830c8bc90f10af54fd8ba35c40aa0b60f
An Nguyen <anndb1903@gmail.com>
2025-05-14 23:16:00 +0700

    about

--- Exercise-4/node-file/file.js
+++ Exercise-4/node-file/file.js
@@ -1,10 +1,34 @@
 const fs = require('fs');
-
-// Đọc nội dung từ file data.json
-fs.readFile('data.json', 'utf8', (err, data) => {
-    if (err) {
-        console.error('Lỗi khi đọc file:', err);
-        return;
-    }
-    console.log('Dữ liệu hiện tại:', data);
-});
+const createFile = async (filename) => {
+  try {
+    await fs.promises.writeFile(filename, 'Hello, World!');
+    console.log(`File '${filename}' created successfully`);
+  } catch (err) {
+    console.error(`Error creating file '${filename}':`, err);
+  }
+};
+const readFile = async (filename) => {
+  try {
+    const data = await fs.promises.readFile(filename, 'utf8');
+    console.log(`Contents of file '${filename}':`, data);
+  } catch (err) {
+    console.error(`Error reading file '${filename}':`, err);
+  }
+};
+const appendToFile = async (filename) => {
+  try {
+    await fs.promises.appendFile(filename, '\nThis is additional content');
+    console.log(`Content appended to file '${filename}' successfully`);
+  } catch (err) {
+    console.error(`Error appending to file '${filename}':`, err);
+  }
+};
+const deleteFile = async (filename) => {
+  try {
+    await fs.promises.unlink(filename);
+    console.log(`File '${filename}' deleted successfully`);
+  } catch (err) {
+    console.error(`Error deleting file '${filename}':`, err);
+  }
+};
+module.exports = {createFile, readFile, appendToFile, deleteFile };
