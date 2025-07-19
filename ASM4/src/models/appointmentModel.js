3e6ddcdbf70be09ab83986632368a2ec3af6ef68
An Nguyen <anndb1903@gmail.com>
2025-07-19 23:50:37 +0700

    update

--- ASM4/src/models/appointmentModel.js
+++ ASM4/src/models/appointmentModel.js
@@ -1,20 +1,9 @@
 const mongoose = require('mongoose');
 
-const timeSlotRegex = /^([01]\d|2[0-3]):([0-5]\d)$/;
-
 const appointmentSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   doctor: { type: mongoose.Schema.Types.ObjectId, ref: 'Doctor', required: true },
-  timeSlot: {
-    type: String,
-    required: true,
-    validate: {
-      validator: function (value) {
-        return timeSlotRegex.test(value);
-      },
-      message: props => `${props.value} is not a valid time slot. Expected format: HH:mm (24h)`
-    }
-  },
+  timeSlot: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
 });
 
